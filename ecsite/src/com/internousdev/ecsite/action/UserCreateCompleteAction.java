00f555ff3ff6ce0fa17cfe40c2e44f4b6f770e1a
REDsky96 <mmnnknm.lk@gmail.com>
2018-12-26 11:12:38 +0900

    2018/12/26

---
+++ ecsite/src/com/internousdev/ecsite/action/UserCreateCompleteAction.java
@@ -0,0 +1,35 @@
+package com.internousdev.ecsite.action;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdev.ecsite.dao.UserCreateCompleteDAO;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserCreateCompleteAction extends ActionSupport implements SessionAware {
+
+
+	private Map<String, Object> session;
+
+	public String execute() throws SQLException{
+		UserCreateCompleteDAO userCreateCompleteDAO = new UserCreateCompleteDAO();
+		userCreateCompleteDAO.createUser(session.get("loginUserId").toString(),
+				session.get("loginPassword").toString(),
+				session.get("userName").toString());//ここの3つはconfirmActionから来ている。この3つが引数となってDAO経由でDBに格納される。
+		String result = SUCCESS;
+		return result;
+	}
+
+
+
+	public Map<String, Object> getSession(){
+		return session;
+	}
+
+	@Override
+	public void setSession(Map<String, Object>session){
+		this.session = session;
+	}
+}
